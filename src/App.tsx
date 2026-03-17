d3cf8e8cf3079c3b3b577452378e43970eea2dc7
calvinthen <samkurniawan1st@gmail.com>
2026-03-17 21:07:17 +0700

    add topbar and styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,23 @@ import Topbar from "./components/Topbar"
 
 function App() {
   return (
-    <div className="">
+    <div className="flex h-screen bg-slate-50 overflow-hidden">
       <Sidebar/>
 
-      <Topbar/>
+      <div className="flex-1 flex flex-col">
+         <Topbar/>
+
+         <main className="flex-1 overflow-y-auto p-8">
+            <h1 className="text-2xl font-bold text-slate-800 mb-6">
+              Dashboard Overview
+            </h1>
+
+            <div className="h-96 border-2 border-dashed border-slate-300 rounded-xl flex items-center justify-center text-slate-400">
+              Temporary data
+            </div>
+         </main>
+      </div>
+
     </div>
   )
 }
--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -2,22 +2,23 @@ import { Bell, Search } from "lucide-react";
 
 const Topbar = () => {
     return (
-        <header>
+        <header className="h-16 bg-white border-b border-slate-200 flex items-center justify-between px-6">
 
             {/* Search Bar */}
-            <div>
+            <div className="flex items-center bg-slate-100 px-4 py-2 rounded-lg w-96 focus-within:ring-2 ring-blue-500 transition">
                 <Search/>
                 <input type="text" 
                 placeholder="Search for orders, products..."
+                className="bg-transparent border-none outline-none ml-2 w-full text-slate-700"
                 />
             </div>
 
-            <div>
-                <button>
-                    <Bell/>
+            <div className="flex items-center gap-4 text-slate-500">
+                <button className="hover:text-blue-600 transition">
+                    <Bell size={20}/>
                 </button>
                 
-                <div>
+                <div className="h-9 w-9 bg-blue-100 text-blue-700 rounded-full flex items-center justify-center font-bold border border-blue-200 cursor-pointer">
                     CT
                 </div>
             </div>
